3167d053929490126c07fae3254ff2d7ce3e3c4f
DagmarMpheio <dagmarmpheiu@gmail.com>
2020-12-29 19:32:24 +0100

    Soma de 1 ate 100

---
+++ src/somade1ate1000/SomaDe1Ate1000.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package somade1ate1000;
+
+/**
+ *
+ * @author dagma
+ */
+public class SomaDe1Ate1000 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i=1, sum=0;
+        for (;i<=1000;i++)
+            sum=sum+i;
+        System.out.println(sum);
+    }
+    
+}
